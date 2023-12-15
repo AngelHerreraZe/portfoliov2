a57f51371b4b258acc4c78c1d16d95fb6aa889dd
Angel Herrera Zermeño <angelherrera1996@gmail.com>
2023-12-15 14:49:30 -0600

    finished about desktop

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,81 +39,8 @@ const About = () => {
         ) : (
           <p>{texts[0].porQueYo.en}</p>
         )}
-        {isSpanish ? <h2>Proyectos destacados</h2> : <h2>Featured projects</h2>}
-        {texts[0].proyectosDestacados.map((text) => (
-          <div key={text.nombre.es}>
-            {isSpanish ? <h3>{text.nombre.es}</h3> : <h3>{text.nombre.en}</h3>}
-            {isSpanish ? (
-              <p>{text.descripcion.es}</p>
-            ) : (
-              <p>{text.descripcion.en}</p>
-            )}
-          </div>
-        ))}
-      </div>
-      <div className="third-item-about">
-        {isSpanish ? <h2>Habilidades técnicas</h2> : <h2>Technical skills</h2>}
-        {isSpanish ? <h3>Frontend</h3> : <h3>Frontend</h3>}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.frontend.es[0]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.frontend.en[0]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.frontend.es[1]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.frontend.en[1]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.frontend.es[2]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.frontend.en[2]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.frontend.es[3]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.frontend.en[3]}</p>
-        )}
-        {isSpanish ? <h3>Backend</h3> : <h3>Backend</h3>}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.backend.es[0]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.backend.en[0]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.backend.es[1]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.backend.en[1]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.backend.es[2]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.backend.en[2]}</p>
-        )}
-        {isSpanish ? <h3>Herramientas y otros</h3> : <h3>Tools and other</h3>}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.es[0]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.en[0]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.es[1]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.en[1]}</p>
-        )}
-        {isSpanish ? (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.es[2]}</p>
-        ) : (
-          <p>{texts[0].habilidadesTecnicas.herramientasYOtros.en[2]}</p>
-        )}
       </div>
       <div className="fourt-item-about">
-        {isSpanish ? <h2>¿Que busco?</h2> : <h2>What am I looking for?</h2>}
-        {isSpanish ? (
-          <p>{texts[0].queBusco.es}</p>
-        ) : (
-          <p>{texts[0].queBusco.en}</p>
-        )}
         {isSpanish ? <h2>Hablemos</h2> : <h2>Let's Talk</h2>}
         {isSpanish ? (
           <p>{texts[0].hablemos.es}</p>
@@ -127,7 +54,7 @@ const About = () => {
           <p>{texts[0].agradecimiento.en}</p>
         )}
       </div>
-      <img src={profile} alt="" className="profile-pic" />
+
       <div className="icons">
         <a href="mailto:angelherrera1996@gmail.com" className="align-text">
           <i className="bx bx-envelope icon"></i>
